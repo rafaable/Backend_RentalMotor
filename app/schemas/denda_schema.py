2a24b671db84d37276b8490f274ea997d2fa72a1
naerizq <naila.artspace@gmail.com>
2026-06-17 14:41:21 +0700

    punyaku biar ga hilang

--- app/schemas/denda_schema.py
+++ app/schemas/denda_schema.py
@@ -4,26 +4,26 @@ from enum import Enum
 
 
 class AlasanDendaGet(str, Enum):
-    kerusakan_kendaraan = "kerusakan_kendaraan"
-    pelanggaran_lalu_lintas = "pelanggaran_lalu_lintas"
-    keterlambatan_pengembalian = "keterlambatan_pengembalian"
+    kerusakan_kendaraan = "kerusakan_kendaraan"
+    pelanggaran_lalu_lintas = "pelanggaran_lalu_lintas"
+    keterlambatan_pengembalian = "keterlambatan_pengembalian"
 
 
 class AlasanDendaManual(str, Enum):
-    kerusakan_kendaraan = "kerusakan_kendaraan"
-    pelanggaran_lalu_lintas = "pelanggaran_lalu_lintas"
+    kerusakan_kendaraan = "kerusakan_kendaraan"
+    pelanggaran_lalu_lintas = "pelanggaran_lalu_lintas"
 
 
 class DendaCreate(BaseModel):
-    id_pengembalian: int
-    id_karyawan: int
-    alasan_denda: AlasanDendaManual
-    nominal_denda: float
-    keterangan: Optional[str] = None
+    id_pengembalian: int
+    id_karyawan: int
+    alasan_denda: AlasanDendaManual
+    nominal_denda: float
+    keterangan: Optional[str] = None
 
 
 class DendaPatch(BaseModel):
-    id_karyawan: Optional[int] = None
-    alasan_denda: Optional[AlasanDendaManual] = None
-    nominal_denda: Optional[float] = None
-    keterangan: Optional[str] = None
+    id_karyawan: Optional[int] = None
+    alasan_denda: Optional[AlasanDendaManual] = None
+    nominal_denda: Optional[float] = None
+    keterangan: Optional[str] = None
